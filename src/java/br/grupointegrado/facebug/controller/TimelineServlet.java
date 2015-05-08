1794a4b9608bfe7db522bdc3fca2dfc423d2a291
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-05-08 20:17:12 -0300

    teste

--- src/java/br/grupointegrado/facebug/controller/TimelineServlet.java
+++ src/java/br/grupointegrado/facebug/controller/TimelineServlet.java
@@ -31,6 +31,7 @@ public class TimelineServlet extends HttpServlet {
          * Neste método devemos carregar todos os dados necessários para exibir na Timeline
          */
         try {
+            String nome;
             Usuario usuario = (Usuario) req.getSession().getAttribute("usuario_logado");
             Connection conn = (Connection) req.getAttribute("conexao");
             List<Postagem> postagens = new PostagemDAO(conn).ultimasPostagens(usuario);
